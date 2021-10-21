bb71f2d469b30c87770f2829c7bbddbb2f1acbb5
Ronald Pineda <rspineda@B131LSL.local>
2021-10-21 16:18:53 +0200

    meme app started

---
+++ Meme-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meme-App
+//
+//  Created by Ronald Pineda on 21/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
