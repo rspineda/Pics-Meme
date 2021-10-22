72c1b2eb62a434e7fd12e30b2312eca8b60356bd
Ronald Pineda <rspineda@B131LSL.local>
2021-10-22 12:00:42 +0200

    added imagePickerController

--- Meme-App/ViewController.swift
+++ Meme-App/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pickImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        present(pickerController, animated: true, completion: nil)
+    }
+    
 }
 
